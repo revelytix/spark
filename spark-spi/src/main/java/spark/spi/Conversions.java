e36a9dabf975d23c218745ad1cba239b2949dc6e
alexhall <alexhall@revelytix.com>
2011-07-27 18:42:03 -0400

    Add conversions from Java objects to RDF literals

--- spark-spi/src/main/java/spark/spi/Conversions.java
+++ spark-spi/src/main/java/spark/spi/Conversions.java
@@ -15,17 +15,161 @@
  */
 package spark.spi;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.net.URI;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import spark.api.rdf.Literal;
+import spark.api.rdf.TypedLiteral;
+import spark.api.uris.XsdTypes;
+import spark.spi.rdf.TypedLiteralImpl;
 import spark.spi.util.DateTime;
 
 /**
- * Class of convenience conversions from lexical string representation to the 
- * various XSD data type native Java representations.
+ * <p>Class of convenience conversions between lexical string representations of the 
+ * various XSD data types and their native Java representations.</p>
+ * 
+ * <p>This class provides methods to translate from native Java to RDF literals, and from RDF
+ * literals and lexical values to native Java objects. In general, translating a native Java object
+ * of a supported XSD datatype to an RDF literal via {@link #toLiteral(Object)} and back to a native
+ * Java object via {@link #toData(TypedLiteral)} will yield an object equal to the original one.
+ * The converse is not necessarily true: translating from RDF typed literal to Java object and back
+ * to RDF typed literal should not be assumed to be a stable round-trip.</p>
+ * 
+ * <p>The following table gives the supported XSD datatypes and their Java counterparts.</p>
+ * 
+ * <table border='1'>
+ *  <tr><th>XSD Datatype</th><th>Java class</th></tr>
+ *  <tr><td><tt>xsd:byte</tt></td><td>{@link Byte}, <tt>byte</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:short</tt></td><td>{@link Short}, <tt>short</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:int</tt></td><td>{@link Integer}, <tt>int</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:long</tt></td><td>{@link Long}, <tt>long</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:boolean</tt></td><td>{@link Boolean}, <tt>boolean</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:float</tt></td><td>{@link Float}, <tt>float</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:double</tt></td><td>{@link Double}, <tt>double</tt> primitive type</td></tr>
+ *  <tr><td><tt>xsd:string</tt></td><td>{@link String}</td></tr>
+ *  <tr><td><tt>xsd:integer</tt></td><td>{@link BigInteger}</td></tr>
+ *  <tr><td><tt>xsd:decimal</tt></td><td>{@link BigDecimal}</td></tr>
+ *  <tr><td><tt>xsd:dateTime</tt></td><td>{@link Date}</td></tr>
+ * </table>
+ * 
+ * <p>Methods are overloaded to use primitive types whenever possible, and boxing/unboxing is done as necessary.</p>
  */
 public final class Conversions {
 
+  private static final Map<URI,Conversion<?>> uriConversions = new HashMap<URI,Conversion<?>>();
+  private static final Map<Class<?>,Conversion<?>> classConversions = new HashMap<Class<?>,Conversion<?>>();
+  
+  /** Datatype conversion class. */
+  private static abstract class Conversion<T> {
+    /** Datatype URI. */
+    final URI typeUri;
+    /** Corresponding Java class. */
+    final Class<T> clazz;
+    /** Initialize and register the class. */
+    Conversion(Class<T> c, URI dt) {
+      typeUri = dt;
+      clazz = c;
+      uriConversions.put(dt, this);
+      classConversions.put(c, this);
+    }
+    /** @return an RDF literal with the appropriate datatype URI and lexical form computed from the given object. */
+    final TypedLiteral literal(Object data) {
+      return new TypedLiteralImpl(lexical(clazz.cast(data)), typeUri);
+    }
+    /** @return a native Java object translated from the given lexical string. */
+    abstract T data(String s);
+    /** @return the lexical string to use for the given object. */
+    String lexical(T data) { return data.toString(); }
+  }
+  
+  /** xsd:byte conversion */
+  private static final Conversion<Byte> BYTE = new Conversion<Byte>(Byte.class, XsdTypes.BYTE) {
+    Byte data(String s) {
+      return toByte(s);
+    }
+  };
+  
+  /** xsd:short conversion */
+  private static final Conversion<Short> SHORT = new Conversion<Short>(Short.class, XsdTypes.SHORT) {
+    Short data(String s) {
+      return toShort(s);
+    }
+  };
+  
+  /** xsd:int conversion */
+  private static final Conversion<Integer> INT = new Conversion<Integer>(Integer.class, XsdTypes.INT) {
+    Integer data(String s) {
+      return toInteger(s);
+    }
+  };
+  
+  /** xsd:long conversion */
+  private static final Conversion<Long> LONG = new Conversion<Long>(Long.class, XsdTypes.LONG) {
+    Long data(String s) {
+      return toLong(s);
+    }
+  };
+  
+  /** xsd:integer conversion */
+  @SuppressWarnings("unused")
+  private static final Conversion<BigInteger> INTEGER = new Conversion<BigInteger>(BigInteger.class, XsdTypes.INTEGER) {
+    BigInteger data(String s) {
+      return toBigInteger(s);
+    }
+  };
+  
+  /** xsd:boolean conversion */
+  private static final Conversion<Boolean> BOOLEAN = new Conversion<Boolean>(Boolean.class, XsdTypes.BOOLEAN) {
+    Boolean data(String s) {
+      return toBoolean(s);
+    }
+  };
+  
+  /** xsd:float conversion */
+  private static final Conversion<Float> FLOAT = new Conversion<Float>(Float.class, XsdTypes.FLOAT) {
+    Float data(String s) {
+      return toFloat(s);
+    }
+  };
+  
+  /** xsd:double conversion */
+  private static final Conversion<Double> DOUBLE = new Conversion<Double>(Double.class, XsdTypes.DOUBLE) {
+    Double data(String s) {
+      return toDouble(s);
+    }
+  };
+  
+  /** xsd:decimal conversion */
+  @SuppressWarnings("unused")
+  private static final Conversion<BigDecimal> DECIMAL = new Conversion<BigDecimal>(BigDecimal.class, XsdTypes.DECIMAL) {
+    BigDecimal data(String s) {
+      return toDecimal(s);
+    }
+  };
+  
+  /** xsd:string conversion */
+  @SuppressWarnings("unused")
+  private static final Conversion<String> STRING = new Conversion<String>(String.class, XsdTypes.STRING) {
+    String data(String s) {
+      return s;
+    }
+  };
+  
+  /** xsd:dateTime conversion */
+  @SuppressWarnings("unused")
+  private static final Conversion<Date> DATE_TIME = new Conversion<Date>(Date.class, XsdTypes.DATE_TIME) {
+    Date data(String s) {
+      return toDateTime(s);
+    }
+    String lexical(Date data) {
+      return DateTime.format(data);
+    }
+  };
+  
   private Conversions() {}
 
   /**
@@ -38,12 +182,39 @@ public final class Conversions {
   }
   
   /**
+   * Convert from lexical to byte (xsd:byte)
+   * @param lexicalByte Lexical representation
+   * @return Converted value
+   */
+  public static byte toByte(String lexicalByte) {
+    return Byte.parseByte(lexicalByte);
+  }
+  
+  /**
+   * Convert from lexical to short (xsd:short)
+   * @param lexicalShort Lexical representation
+   * @return Converted value
+   */
+  public static short toShort(String lexicalShort) {
+    return Short.parseShort(lexicalShort);
+  }
+  
+  /**
    * Convert from lexical to int (xsd:int)
    * @param lexicalInteger Lexical representation
    * @return Converted value
    */
   public static int toInteger(String lexicalInteger) {
     return Integer.parseInt(lexicalInteger);
+  }
+  
+  /**
+   * Convert from lexical to long (xsd:long)
+   * @param lexicalLong Lexical representation
+   * @return Converted value
+   */
+  public static long toLong(String lexicalLong) {
+    return Long.parseLong(lexicalLong);
   }
   
   /**
@@ -72,6 +243,15 @@ public final class Conversions {
   public static double toDouble(String lexicalDouble) {
     return Double.parseDouble(lexicalDouble);
   }
+  
+  /**
+   * Convert from lexical to double (xsd:double)
+   * @param lexicalDecimal Lexical representation
+   * @return Converted value
+   */
+  public static BigDecimal toDecimal(String lexicalDecimal) {
+    return new BigDecimal(lexicalDecimal);
+  }
 
   /**
    * Convert from lexical to Date (xsd:dateTime)
@@ -80,5 +260,109 @@ public final class Conversions {
    */
   public static Date toDateTime(String lexicalDate) {
     return DateTime.parse(lexicalDate);
+  }
+  
+  /**
+   * Convert from RDF literal to native Java object.
+   * @param lit RDF literal.
+   * @return Java object converted from the literal.
+   */
+  public static Object toData(Literal lit) {
+    if (lit == null) throw new IllegalArgumentException("Can't convert null literal");
+    if (lit instanceof TypedLiteral) return toData((TypedLiteral)lit);
+    // Untyped literals are xsd:string
+    // Note this isn't strictly correct; language tags will be lost here.
+    return lit.getLexical();
+  }
+  
+  /**
+   * Convert from RDF typed literal to native Java object.
+   * @param lit RDF typed literal
+   * @return Java object converted from the lexical value based on the mappings specified by the literal datatype URI.
+   */
+  public static Object toData(TypedLiteral lit) {
+    if (lit == null) throw new IllegalArgumentException("Can't convert null literal");
+    Conversion<?> c = uriConversions.get(lit.getDataType());
+    if (c == null) throw new IllegalArgumentException("Don't know how to convert literal of type " + lit.getDataType());
+    return c.data(lit.getLexical());
+  }
+  
+  /**
+   * Convert from byte to xsd:byte typed literal
+   * @param b Byte value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(byte b) {
+    return new TypedLiteralImpl(Byte.toString(b), BYTE.typeUri);
+  }
+  
+  /**
+   * Convert from short to xsd:short typed literal
+   * @param s Short value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(short s) {
+    return new TypedLiteralImpl(Short.toString(s), SHORT.typeUri);
+  }
+
+  /**
+   * Convert from integer to xsd:int typed literal
+   * @param i Integer value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(int i) {
+    return new TypedLiteralImpl(Integer.toString(i), INT.typeUri);
+  }
+  
+  /**
+   * Convert from long to xsd:long typed literal
+   * @param l Long value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(long l) {
+    return new TypedLiteralImpl(Long.toString(l), LONG.typeUri);
+  }
+  
+  /**
+   * Convert from boolean to xsd:boolean typed literal
+   * @param b Boolean value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(boolean b) {
+    return new TypedLiteralImpl(Boolean.toString(b), BOOLEAN.typeUri);
+  }
+  
+  /**
+   * Convert from float to xsd:float typed literal
+   * @param f Float value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(float f) {
+    return new TypedLiteralImpl(Float.toString(f), FLOAT.typeUri);
+  }
+  
+  /**
+   * Convert from double to xsd:double typed literal
+   * @param d Double value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(double d) {
+    return new TypedLiteralImpl(Double.toString(d), DOUBLE.typeUri);
+  }
+  
+  /**
+   * Convert from an arbitrary Java object to an RDF typed literal, using an XSD datatype if possible.
+   * @param value Object value
+   * @return Converted literal
+   */
+  public static TypedLiteral toLiteral(Object value) {
+    if (value == null) throw new IllegalArgumentException("Can't convert null value");
+    Conversion<?> c = classConversions.get(value.getClass());
+    if (c != null) return c.literal(value);
+    // The object has an unrecognized type that doesn't translate directly to XSD.
+    // Omitting the datatype would imply a type of xsd:string, so use xsd:anySimpleType instead.
+    // The use of xsd:anySimpleType prevents round-tripping; in the future we could possibly
+    // serialize this as a byte array and use xsd:hexBinary or xsd:base64Binary.
+    return new TypedLiteralImpl(value.toString(), XsdTypes.ANY_SIMPLE_TYPE);
   }
 }
---
+++ spark-spi/src/test/java/spark/spi/TestConversions.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright © 2011 Revelytix, Inc.  All rights reserved.
+ */
+package spark.spi;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URI;
+import java.util.Date;
+
+import junit.framework.TestCase;
+import spark.api.rdf.TypedLiteral;
+import spark.api.uris.XsdTypes;
+import spark.spi.rdf.PlainLiteralImpl;
+import spark.spi.rdf.TypedLiteralImpl;
+
+/**
+ * @author Alex Hall
+ * @date Jul 27, 2011
+ */
+public class TestConversions extends TestCase {
+
+  private static final boolean DEBUG = false;
+  
+  public void testInt() {
+    String s = "1234";
+    int i = 1234;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.INT);
+    assertEquals(i, Conversions.toInteger(s));
+    assertEquals(l, Conversions.toLiteral(i));
+    roundTrip(Integer.valueOf(i), l);
+    invalid("abc", XsdTypes.INT);
+    invalid("156432418974561566571", XsdTypes.INT);
+  }
+  
+  public void testInteger() {
+    String s = "156432418974561566571";
+    BigInteger i = new BigInteger(s);
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.INTEGER);
+    assertEquals(i, Conversions.toBigInteger(s));
+    assertEquals(l, Conversions.toLiteral(i));
+    roundTrip(i, l);
+    invalid("NaN", XsdTypes.INTEGER);
+  }
+  
+  public void testBoolean() {
+    String s = "true";
+    boolean b = true;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.BOOLEAN);
+    assertEquals(b, Conversions.toBoolean(s));
+    assertEquals(l, Conversions.toLiteral(b));
+    roundTrip(Boolean.valueOf(b), l);
+  }
+  
+  public void testByte() {
+    String s = "99";
+    byte b = 99;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.BYTE);
+    assertEquals(b, Conversions.toByte(s));
+    assertEquals(l, Conversions.toLiteral(b));
+    roundTrip(Byte.valueOf(b), l);
+    invalid("256", XsdTypes.BYTE);
+  }
+  
+  public void testShort() {
+    String s = "25943";
+    short i = 25943;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.SHORT);
+    assertEquals(i, Conversions.toShort(s));
+    assertEquals(l, Conversions.toLiteral(i));
+    roundTrip(Short.valueOf(i), l);
+    invalid("45256", XsdTypes.SHORT);
+  }
+  
+  public void testLong() {
+    String s = "126476513274";
+    long i = 126476513274L;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.LONG);
+    assertEquals(i, Conversions.toLong(s));
+    assertEquals(l, Conversions.toLiteral(i));
+    roundTrip(Long.valueOf(i), l);
+    invalid("156432418974561566571", XsdTypes.LONG);
+  }
+  
+  public void testFloat() {
+    String s = "5643.475";
+    float f = 5643.475f;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.FLOAT);
+    assertEquals(f, Conversions.toFloat(s));
+    assertEquals(l, Conversions.toLiteral(f));
+    roundTrip(Float.valueOf(f), l);
+    invalid("86nd", XsdTypes.FLOAT);
+  }
+  
+  public void testDouble() {
+    String s = "8734539.39586345";
+    double d = 8734539.39586345d;
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.DOUBLE);
+    assertEquals(d, Conversions.toDouble(s));
+    assertEquals(l, Conversions.toLiteral(d));
+    roundTrip(Double.valueOf(d), l);
+    invalid("86nd", XsdTypes.DOUBLE);
+  }
+  
+  public void testDecimal() {
+    String s = "8734539.39586345";
+    BigDecimal d = new BigDecimal(s);
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.DECIMAL);
+    assertEquals(d, Conversions.toDecimal(s));
+    roundTrip(d, l);
+    invalid("86nd", XsdTypes.DECIMAL);
+  }
+  
+  public void testDate() {
+    String s = "2011-07-27T22:25:16.812Z";
+    Date d = new Date(1311805516812L);
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.DATE_TIME);
+    assertEquals(d, Conversions.toDateTime(s));
+    roundTrip(d, l);
+    invalid("Wed Jul 27 18:25:16 EDT 2011", XsdTypes.DATE_TIME);
+  }
+  
+  public void testString() {
+    String s = "foo bar";
+    TypedLiteralImpl l = new TypedLiteralImpl(s, XsdTypes.STRING);
+    assertEquals(s, Conversions.toData(new PlainLiteralImpl(s)));
+    roundTrip(s, l);
+  }
+  
+  public void testUnknownType() {
+    invalid("1234", XsdTypes.UNSIGNED_INT);
+  }
+  
+  private static void roundTrip(Object val, TypedLiteral lit) {
+    assertEquals(val, Conversions.toData(lit));
+    assertEquals(lit, Conversions.toLiteral(val));
+  }
+  
+  private static void invalid(String lexical, URI datatype) {
+    TypedLiteralImpl l = new TypedLiteralImpl(lexical, datatype);
+    try {
+      Conversions.toData(l);
+      fail("Should have thrown exception converting literal " + l);
+    } catch (Exception e) {
+      if (DEBUG) System.out.println(e.getMessage());
+    }
+  }
+}
