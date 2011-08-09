554a2265096d460ec9d79f76261645e95cfe60fd
alexhall <alexhall@revelytix.com>
2011-08-09 14:23:54 -0400

    Test cases for SPARQL XML metadata links

--- spark-http-client/src/test/java/spark/protocol/parser/TestXMLSelectResults.java
+++ spark-http-client/src/test/java/spark/protocol/parser/TestXMLSelectResults.java
@@ -23,11 +23,18 @@ import static spark.spi.TestCursor.NONE;
 
 import java.io.FileInputStream;
 import java.net.URI;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.List;
 
 import junit.framework.TestCase;
+import spark.api.Command;
+import spark.api.Connection;
 import spark.api.Solutions;
+import spark.api.credentials.NoCredentials;
 import spark.api.uris.XsdTypes;
+import spark.protocol.ProtocolDataSource;
+import spark.protocol.ProtocolResult;
 import spark.spi.TestCursor;
 import spark.spi.rdf.BlankNodeImpl;
 import spark.spi.rdf.NamedNodeImpl;
@@ -45,15 +52,22 @@ public class TestXMLSelectResults extends TestCase {
   private static final String TEST_DIR = "src/test/resources/sparql-xml/";
   private static final String FILE_EXT = ".xml";
   
-  private static Solutions getTestData(String testName) throws Exception {
+  private static Solutions getTestData(Command cmd, String testName) throws Exception {
     String fn = TEST_DIR + testName + FILE_EXT;
-    return (Solutions) XMLResultsParser.createResults(null, new FileInputStream(fn), null);
+    return (Solutions) XMLResultsParser.createResults(cmd, new FileInputStream(fn), null);
+  }
+  
+  private static Solutions getTestData(String testName) throws Exception {
+    return getTestData(null, testName);
   }
   
   public void testEmptyResults() throws Exception {
     Solutions s = getTestData("empty-results");
     assertNotNull(s);
     try {
+      assertTrue(s instanceof ProtocolResult);
+      assertTrue(((ProtocolResult)s).getMetadata().isEmpty());
+      
       assertEquals(Arrays.asList("foo", "bar"), s.getVariables());
       
       // Check cursor methods.
@@ -74,6 +88,9 @@ public class TestXMLSelectResults extends TestCase {
     Solutions s = getTestData("single-result");
     assertNotNull(s);
     try {
+      assertTrue(s instanceof ProtocolResult);
+      assertTrue(((ProtocolResult)s).getMetadata().isEmpty());
+      
       assertEquals(Arrays.asList("x", "y", "z"), s.getVariables());
       
       // Check cursor methods.
@@ -100,6 +117,9 @@ public class TestXMLSelectResults extends TestCase {
     Solutions s = getTestData("sparql-results");
     assertNotNull(s);
     try {
+      assertTrue(s instanceof ProtocolResult);
+      assertTrue(((ProtocolResult)s).getMetadata().isEmpty());
+      
       String var = "a";
       assertEquals(Arrays.asList(var), s.getVariables());
       
@@ -133,6 +153,52 @@ public class TestXMLSelectResults extends TestCase {
       TestCursor.assertCursor(s, AFTER_LAST);
     } finally {
       s.close();
+    }
+  }
+  
+  static void metadataTest(Command cmd, String testName, String... metadata) throws Exception {
+    Solutions s = getTestData(cmd, testName);
+    try {
+      assertNotNull(s);
+      assertTrue(s instanceof ProtocolResult);
+      List<String> md = ((ProtocolResult)s).getMetadata();
+      assertNotNull(md);
+      assertEquals(Arrays.asList(metadata), md);
+      
+      String varX = "x";
+      String varY = "y";
+      assertEquals(Arrays.asList(varX, varY), s.getVariables());
+      
+      TestCursor.assertCursor(s, BEFORE_FIRST);
+      
+      assertTrue(s.next());
+      TestCursor.assertCursor(s, FIRST | LAST);
+      assertEquals(URI.create("http://example.org/foo"), s.getURI(varX));
+      assertEquals("bar", s.getString(varY));
+      
+      assertFalse(s.next());
+      TestCursor.assertCursor(s, AFTER_LAST);
+    } finally {
+      s.close();
+    }
+  }
+  
+  public void testMetadata() throws Exception {
+    URL serviceUrl = new URL("http://example.org/sparql");
+    // dummy data source to pass the URL down to the parser.
+    ProtocolDataSource ds = new ProtocolDataSource(serviceUrl);
+    try {
+      Connection c = ds.getConnection(NoCredentials.INSTANCE);
+      Command cmd = c.createCommand("SELECT foo"); // query isn't actually executed.
+      
+      metadataTest(cmd, "results-with-metadata",
+          "http://sample.org/metadata.rdf", "http://example.org/service-description.rdf");
+      metadataTest(cmd, "results-with-base-uri",
+          "http://sample.org/metadata.rdf", "http://revelytix.com/ns/service-description.rdf", "http://example.com/sparql.ttl");
+      metadataTest(cmd, "results-with-bad-link",
+          "http://sample.org/metadata.rdf", "http://example.org/service-description.rdf");
+    } finally {
+      ds.close();
     }
   }
 }
